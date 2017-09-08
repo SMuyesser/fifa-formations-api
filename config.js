aab833c96086298f14ab18325c16a4d9305d4630
SMuyesser <smuyesser@gmail.com>
2017-09-08 15:34:49 -0700

    Formation Model Complete

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
-exports.CLIENT_ORIGIN = 'http://localhost:3000';
+exports.CLIENT_ORIGIN = 'http://localhost:3000' || 'https://fifaformations.netlify.com/';
 exports.PORT = process.env.PORT || 8000;
+
--- models/formationSchema.js
+++ models/formationSchema.js
@@ -1,1 +1,82 @@
-formationSchema
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const FormationSchema = mongoose.Schema({
+	formation: {
+		type: String,
+		required: true
+	},
+	strength: [{
+		type: String,
+	}],
+	weakness: [{
+		type: String,
+	}],
+	tactics: {
+		buildUp: {
+			speed: {
+				type: Number
+			},
+			passing: {
+				type: Number
+			},
+			positioning: {
+				type: String
+			}
+		},
+		chanceCreation: {
+			passing: {
+				type: Number
+			},
+			shooting: {
+				type: Number
+			},
+			crossing: {
+				type: Number
+			},
+			positioning: {
+				type: String				
+			}
+		},
+		defence: {
+			pressure: {
+				type: Number
+			},
+			aggression: {
+				type: Number
+			},
+			teamWidth: {
+				type: Number
+			},
+			defenderLine: {
+				type: String
+			}
+		}
+	},
+	playerInstructions: {
+		fwd: [{
+			type: String
+		}],
+		mid: [{
+			type: String
+		}],
+		def: [{
+			type: String
+		}]
+	}
+});
+
+FormationSchema.methods.apiRepr = function() {
+	return {
+		formation: this.formation,
+		strength: [...this.strength],
+		weakness: [...this.weakness],
+		tactics: this.tactics,
+		playerInstructions: this.playerInstructions
+	};
+};
+
+const Formation = mongoose.model('Formation', FormationSchema);
+
+module.exports = {Formation};
