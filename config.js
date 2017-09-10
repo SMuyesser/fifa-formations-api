4f000919d0006797b9293bfc5fe6a604708d3f4d
SMuyesser <smuyesser@gmail.com>
2017-09-09 21:10:48 -0700

    updated json data

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-exports.CLIENT_ORIGIN = 'http://localhost:3000' || 'https://fifaformations.netlify.com/';
+exports.CLIENT_ORIGIN = 'https://fifaformations.netlify.com';
 exports.PORT = process.env.PORT || 8000;
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/fifaformationsdb';
-
