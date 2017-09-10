99d01514d477f818d029c77fee10a24d9ef9e67f
SMuyesser <smuyesser@gmail.com>
2017-09-09 21:30:06 -0700

    config update

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
-exports.CLIENT_ORIGIN = 'https://fifaformations.netlify.com';
+exports.CLIENT_ORIGIN = 'http://localhost:3000';
 exports.PORT = process.env.PORT || 8000;
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/fifaformationsdb';
+
+/* || 'https://fifaformations.netlify.com/'*/
--- routes/formations.js
+++ routes/formations.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const app = express();
 
 const {Formation} = require('../models/formationSchema');
+
+// CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
 
 //get a single formation's info
 router.get('/:formation', (req, res) => {
